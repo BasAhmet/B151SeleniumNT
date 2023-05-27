f9d3718221d5cf6aadb8f28567211e57b8e8e550
Ahmet Baş <ahba8184@gmail.com>
2023-05-27 23:23:32 +0300

    Selenium ikinci gün

--- src/test/java/techproed/day02_DriverMethods/C04_ManageMethods_ImplicitlyWait.java
+++ src/test/java/techproed/day02_DriverMethods/C04_ManageMethods_ImplicitlyWait.java
@@ -14,30 +14,24 @@ public class C04_ManageMethods_ImplicitlyWait {
 
         //techproed sayfasina gidelim
         driver.get("https://techproeducation.com");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
         //amazona gidelim
         driver.get("https://www.amazon.com/");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
         //techpro sayfasina geri donelim
         driver.navigate().back();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
         //sayfa basligini techpro icerdigini test edelim
         String actualTitleTech = driver.getTitle();
-        String expectedTitleTech = "techpro" ;
+        String expectedTitleTech = "Techpro" ;
         if (actualTitleTech.contains(expectedTitleTech)) {
             System.out.println("Test Passed");
         }else {
             System.out.println("Test FAILED");
         }
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
 
         //tekrar amazon sayfasina gidelim
         driver.navigate().forward();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
-
 
         //sayfa basligini Amazon icerdigini test edelim
         String actualTitleAmazon = driver.getTitle();
@@ -47,7 +41,6 @@ public class C04_ManageMethods_ImplicitlyWait {
         }else {
             System.out.println("Test FAILED");
         }
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         Thread.sleep(3000);
 
         driver.close();
